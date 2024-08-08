c6d0f83f02f75c8997075502ef65527a65046049
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-08-08 14:57:20 -0700

    Added value state to todo list entry

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TodoForm = () => {
-  return <div>TodoForm</div>;
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Please Input Task"
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
 };
