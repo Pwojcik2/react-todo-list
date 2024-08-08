503f1e01974edc12b6048593e80101789b81a493
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-08-08 15:35:08 -0700

    Added props for adding tasks to display on list

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    addTodo(value);
+    setValue("");
   };
   return (
     <form className="TodoForm" onSubmit={handleSubmit}>
       <input
         type="text"
         className="todo-input"
+        value={value}
         placeholder="Please Input Task"
         onChange={(e) => setValue(e.target.value)}
       ></input>
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { ToDo } from "./ToDo";
+
+uuidv4();
 
 export const TodoWrapper = () => {
-  return <div>TodoWrapper</div>;
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
+  return (
+    <div className="TodoWrapper">
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <ToDo task={todo} key={index} />
+      ))}
+    </div>
+  );
 };
